69abebd3a5468faf2cb20e1161b961bdd69b768e
Hakan Taşkirmaz <doritosvslays@gmail.com>
2021-09-01 15:35:35 +0200

    Changed response into a readStream with type image

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import path from "path"
+import fs from 'fs'
 import bodyParser from "express"
+import stream from 'stream'
 // @ts-ignore
 import emojiUnicode from "emoji-unicode"
 import { createCanvas, loadImage } from "canvas"
@@ -16,7 +18,7 @@ const canvas_height = 1600
 
 app.use("/", express.static(path.join(__dirname, "client")))
 
-app.post("/api/generate-cover", async (req, res) => {
+app.post("/api/generate-cover", async (req, res) => {  
   const unicodes = req.body as string[]
   const canvas = createCanvas(canvas_width, canvas_height)
   const ctx = canvas.getContext("2d")
@@ -26,23 +28,27 @@ app.post("/api/generate-cover", async (req, res) => {
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    const stream = canvas.toBuffer()
+    const buffer = canvas.toBuffer()
     res.writeHead(200, { "Content-Type": `image/png` })
-    return res.end(stream)
+    return res.end(buffer)
   }
 
-  const emoji_array: string[] = []
+	const emoji_array: string[] = []
 
   unicodes.map((unicode) => {
-    // Code_points are basically the raw emoji_urls
-    // for example: 1f469-200d-1f393
-    const code_points: string = emojiUnicode(unicode)
-    const code_points_joined = code_points.split(" ").join("-")
-    const path = `${code_points_joined}.png`
-
-    if (emoji_array.length <= 11) {
-      emoji_array.push(path)
-    }
+    if (unicode !== "" || unicode.length > 0) {
+			// Code_points are basically the raw emoji_urls
+			// for example: 1f469-200d-1f393
+			const code_points: string = emojiUnicode(unicode)
+			const code_points_joined = code_points.split(" ").join("-")
+			const path = `${code_points_joined}.png`
+	
+			if (emoji_array.length <= 11) {
+				emoji_array.push(path)
+			}
+    } else {
+			emoji_array.push("")
+		}
   })
 
   // DRAKE
@@ -66,30 +72,37 @@ app.post("/api/generate-cover", async (req, res) => {
 
   await Promise.all(
     emoji_array.map(async (emoji_url, index) => {
-      await axios
-        .get(`https://raw.githubusercontent.com/iamcal/emoji-data/master/img-apple-160/${emoji_url}`, {
-          responseType: "arraybuffer"
-        })
-        .then((r) => {
-          const emoji_buffer = Buffer.from(r.data, "binary")
-
-          loadImage(emoji_buffer)
-            .then((image) => ctx.drawImage(image, locations[index].x, locations[index].y, 270, 270))
-            .catch((e) => {
-              console.log(e)
-              return res.sendStatus(500)
-            })
-        })
-        .catch((e) => {
-          console.log(e)
-          return res.sendStatus(404)
-        })
+			if (emoji_url !== "" || emoji_url.length > 0) {
+				await axios
+					.get(`https://raw.githubusercontent.com/iamcal/emoji-data/master/img-apple-160/${emoji_url}`, {
+						responseType: "arraybuffer"
+					})
+					.then((r) => {
+						const emoji_buffer = Buffer.from(r.data, "binary")
+	
+						loadImage(emoji_buffer)
+							.then((image) => ctx.drawImage(image, locations[index].x, locations[index].y, 270, 270))
+							.catch((e) => {
+								console.log(e)
+								return res.sendStatus(500)
+							})
+					})
+					.catch((e) => {
+						console.log(e)
+						return res.sendStatus(404)
+					})
+			}
     })
   )
 
-  const stream = canvas.toBuffer()
-  res.writeHead(200, { "Content-Type": `image/png` })
-  return res.end(stream)
+  const buffer = canvas.toBuffer()
+  const readStream = new stream.PassThrough()
+  readStream.end(buffer);  
+  res.setHeader('Content-disposition', 'attachment; filename="clb.png"');
+  res.setHeader('Content-type', 'image/png');
+  
+  readStream.pipe(res);
+
 })
 
 app.listen(port, () => console.log(`server started at http://localhost:${port}`))
