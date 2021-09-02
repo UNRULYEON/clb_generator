caa7631083af52776a45e633bd6e5d41ab3695b8
Amar Kisoensingh <amar@kisoensingh.sh>
2021-09-02 21:15:25 +0200

    Added source website for useTheme hook

--- client/src/components/useTheme/index.tsx
+++ client/src/components/useTheme/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+// https://medium.com/hypersphere-codes/detecting-system-theme-in-javascript-css-react-f6b961916d48
+
 export type userPreference = {
 	theme: 'light' | 'dark' | 'system'
 }
