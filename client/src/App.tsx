c4fb309fe852d9d36183cf8c62d98fbd01b233c8
Amar Kisoensingh <amar@kisoensingh.sh>
2021-09-02 13:48:27 +0200

    Made useThemeDetector hook its own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import CoverGrid from './components/CoverGrid'
 import Switch from '@material-ui/core/Switch'
+import useThemeDetector from './components/useThemeDetector'
 
 type userPreference = {
 	theme: 'light' | 'dark' | 'system'
-}
-
-const useThemeDetector = () => {
-	const getCurrentTheme = () =>
-		window.matchMedia('(prefers-color-scheme: dark)').matches
-	const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme())
-	const mqListener = (e: { matches: boolean }) => {
-		setIsDarkTheme(e.matches)
-	}
-
-	useEffect(() => {
-		const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
-		darkThemeMq.addEventListener('change', mqListener)
-		return () => darkThemeMq.removeEventListener('change', mqListener)
-	}, [])
-
-	return isDarkTheme
 }
 
 function App() {
